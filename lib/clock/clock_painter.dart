438bb6406b601002c0fbf226583c75dfaef0efd3
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-16 21:16:48 +0100

    Added clock_painter.dart

---
+++ lib/clock/clock_painter.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ClockPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
