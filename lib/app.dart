b1d51991334ebde4f99b46c28c6df6fc9ae8d5d8
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-16 21:16:30 +0100

    Added app.dart

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'clock/clock.dart';
+
+class ClockApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Clock(),
+    );
+  }
+}
