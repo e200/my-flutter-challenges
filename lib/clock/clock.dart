6ba5ced5ab26e45a133722baf7130c411d3ef2b9
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-16 21:16:58 +0100

    Added clock.dart

---
+++ lib/clock/clock.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Clock extends StatefulWidget {
+  @override
+  _ClockState createState() => _ClockState();
+}
+
+class _ClockState extends State<Clock> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //
+    );
+  }
+}
