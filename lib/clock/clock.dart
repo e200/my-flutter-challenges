f201f8fd4609765e9e32c0289bc6f9fe9d10e996
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-17 05:04:42 +0100

    Added clock.dart

--- lib/clock/clock.dart
+++ lib/clock/clock.dart
@@ -1,3 +1,4 @@
+import 'package:clock/clock/clock_painter.dart';
 import 'package:flutter/material.dart';
 
 class Clock extends StatefulWidget {
@@ -8,8 +9,15 @@ class Clock extends StatefulWidget {
 class _ClockState extends State<Clock> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: CustomPaint(
+            size: Size.square(200),
+            painter: ClockPainter(),
+          ),
+        ),
+      ),
     );
   }
 }
