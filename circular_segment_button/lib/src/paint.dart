5b8c64049c5ac55684262fbf578ed52cfec95c4c
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-21 22:47:09 +0100

    Added paint.dart

---
+++ circular_segment_button/lib/src/paint.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class CircularSegmentPaint extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+}
