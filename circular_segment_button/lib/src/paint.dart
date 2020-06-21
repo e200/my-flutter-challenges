486c264f8a60432058752d8288a29707c28fb221
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-21 23:01:36 +0100

    Added code that draws the arcs

--- circular_segment_button/lib/src/paint.dart
+++ circular_segment_button/lib/src/paint.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/widgets.dart';
 
 class CircularSegmentPaint extends CustomPainter {
+  final int segmentsCount = 4;
+
+  Paint _segmentPaint;
+
+  CircularSegmentPaint() {
+    _segmentPaint = Paint()
+    ..strokeCap = StrokeCap.round;
+  }
+
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    final _centerX = size.width / 2;
+    final _centerY = size.width / 2;
+    final _center = Offset(_centerX, _centerY);
+
+    for (int i = 0; i < segmentsCount; i++) {
+      canvas.drawArc(
+        Rect.fromCenter(
+          center: _center,
+          width: size.width,
+          height: size.height,
+        ),
+        0,
+        0,
+        false,
+        _segmentPaint,
+      );
+    }
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    throw UnimplementedError();
+    return true;
   }
 }
