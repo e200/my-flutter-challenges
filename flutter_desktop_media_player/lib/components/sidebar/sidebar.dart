4eb6399528ae33fd3e7ea296880a3541105b8daa
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-24 23:04:08 +0100

    Added sidebar

---
+++ flutter_desktop_media_player/lib/components/sidebar/sidebar.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_desktop_media_player/components/sidebar/sidebar_cover.dart';
+import 'package:flutter_desktop_media_player/components/sidebar/sidebar_tile.dart';
+import 'package:line_icons/line_icons.dart';
+
+class Sidebar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 280,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            height: 300,
+            padding: EdgeInsets.all(15),
+            child: ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                SidebarTile(
+                  icon: LineIcons.home,
+                  title: 'Home',
+                ),
+                SidebarTile(
+                  icon: LineIcons.inbox,
+                  title: 'Browse',
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SidebarTile(
+                  title: 'Library',
+                ),
+                SidebarTile(
+                  title: 'Playlists',
+                  isReadOnly: true,
+                ),
+                Column(
+                  children: <Widget>[
+                    SidebarTile(
+                      title: 'Pure fight!!1 🔥',
+                      isActive: true,
+                    ),
+                    SidebarTile(
+                      title: 'Inspired 🦄',
+                      isActive: true,
+                    ),
+                    SidebarTile(
+                      title: 'Nostalgia',
+                      isActive: true,
+                    ),
+                    SidebarTile(
+                      title: 'Calm',
+                      isActive: true,
+                    ),
+                    SidebarTile(
+                      title: 'Calm',
+                      isActive: true,
+                    ),
+                    SidebarTile(
+                      title: 'Archive',
+                      isActive: true,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SidebarCover(
+                  image: 'assets/now_playing.jpg',
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 15,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'So Heavy I Fell Through the Earth',
+                        style: TextStyle(
+                          color: Color(0xFFC4C1C2),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Grimes',
+                        style: TextStyle(
+                          color: Color(0xFF4E586B),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
