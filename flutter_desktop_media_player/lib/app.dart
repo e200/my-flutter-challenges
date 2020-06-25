a06a81ab95e93991f3cae35042ef7dead0362df0
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-25 22:52:34 +0100

    Added customtext theme

--- flutter_desktop_media_player/lib/app.dart
+++ flutter_desktop_media_player/lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_desktop_media_player/components/playlist/widget.dart';
+import 'package:flutter_desktop_media_player/components/player/player.dart';
 import 'package:flutter_desktop_media_player/components/sidebar/sidebar.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,179 +9,27 @@ class FlutterDesktopMediaPlayerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Desktop Media Player',
       theme: ThemeData(
-        textTheme: GoogleFonts.sourceSansProTextTheme(),
+        textTheme: GoogleFonts.sourceSansProTextTheme().copyWith(
+          headline1: TextStyle(
+            fontSize: 72.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline6: TextStyle(
+            fontSize: 36.0,
+          ),
+          bodyText2: TextStyle(
+            color: Color(0xFFC4C1C2),
+            fontSize: 14.0,
+          ),
+        ),
       ),
       home: Scaffold(
         backgroundColor: Color(0xFF0F141D),
         body: Builder(builder: (context) {
           return Column(
             children: <Widget>[
-              Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Sidebar(),
-                    Expanded(
-                      child: Playlist()
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(30),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Row(),
-                        ),
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(15),
-                                child: Icon(
-                                  Icons.skip_previous,
-                                  color: Color(0xFF303846),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF1F2631),
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                child: Icon(
-                                  Icons.pause,
-                                  size: 28,
-                                  color: Color(0xFFC4C1C2),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(15),
-                                child: Icon(
-                                  Icons.skip_next,
-                                  color: Color(0xFF303846),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(15),
-                                child: Icon(
-                                  Icons.shuffle,
-                                  color: Color(0xFF303846),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(15),
-                                child: Icon(
-                                  Icons.repeat,
-                                  color: Color(0xFF616E84),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(15),
-                                child: Icon(
-                                  Icons.volume_up,
-                                  color: Color(0xFF303846),
-                                ),
-                              ),
-                              Stack(
-                                children: <Widget>[
-                                  Container(
-                                    height: 4,
-                                    width: 80,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF303846),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  Container(
-                                    height: 4,
-                                    width: 50,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF616E84),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Stack(
-                                children: <Widget>[
-                                  Container(
-                                    height: 4,
-                                    width:
-                                        MediaQuery.of(context).size.width - 60,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF303846),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  Container(
-                                    height: 4,
-                                    width: MediaQuery.of(context).size.width /
-                                            1.50 -
-                                        60,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF616E84),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Container(
-                                width: MediaQuery.of(context).size.width - 60,
-                                child: Row(
-                                  children: <Widget>[
-                                    Text(
-                                      '2:15',
-                                      style: TextStyle(
-                                        color: Color(0xFF616E84),
-                                      ),
-                                    ),
-                                    Spacer(),
-                                    Text(
-                                      '3:51',
-                                      textAlign: TextAlign.end,
-                                      style: TextStyle(
-                                        color: Color(0xFF616E84),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              Sidebar(),
+              Player(),
             ],
           );
         }),
