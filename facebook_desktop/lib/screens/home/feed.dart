9053471eada725a678e8ff64e244603271176f45
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-27 23:40:39 +0100

    Added feed

---
+++ facebook_desktop/lib/screens/home/feed.dart
@@ -0,0 +1,22 @@
+import 'package:facebook_desktop/screens/home/components/section.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Feed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Section(
+      title: SectionTitle(
+        title: 'STORIES',
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          children: [
+            //
+          ],
+        ),
+      ),
+    );
+  }
+}
