34b26ed04f14f96d0bf00be2e2679aec6b3ae3d1
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-28 20:44:28 +0100

    Improved widget structure

--- facebook_desktop/lib/screens/home/feed/widget.dart
+++ facebook_desktop/lib/screens/home/feed/widget.dart
@@ -1,4 +1,8 @@
+import 'package:facebook_desktop/components/post.dart';
+import 'package:facebook_desktop/screens/home/components/poster.dart';
 import 'package:facebook_desktop/screens/home/feed/stories/widget.dart';
+import 'package:facebook_desktop/screens/home/feed/suggested_friends/widget.dart';
+import 'package:facebook_desktop/screens/home/feed/your_friends/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -6,16 +10,39 @@ class Feed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
       children: [
         Stories(),
         Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              children: [
-                // Poster(),
-              ],
-            )
+            Flexible(
+              flex: 2,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: Poster(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: Post(),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 30,),
+            Flexible(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  YourFriends(),
+                  SuggestedFriends(),
+                ],
+              ),
+            ),
           ],
         ),
       ],
