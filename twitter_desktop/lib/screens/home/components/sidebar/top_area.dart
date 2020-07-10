794d2f5f78ded434b1a3450dc7a4390a39e2de3d
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-10 16:57:57 +0100

    Added SidebarTopArea

---
+++ twitter_desktop/lib/screens/home/components/sidebar/top_area.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SidebarTopArea extends StatelessWidget {
+  final EdgeInsets padding;
+
+  const SidebarTopArea({
+    Key key,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding,
+      child: Image.asset(
+        'resources/images/twitter_logo.png',
+        fit: BoxFit.contain,
+        width: 50,
+        height: 50,
+      ),
+    );
+  }
+}
