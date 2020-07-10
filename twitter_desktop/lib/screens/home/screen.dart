e2ef5ded0a35a7f0c11ec7642bccc5e41b0f5230
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-10 16:57:09 +0100

    Renamed

---
+++ twitter_desktop/lib/screens/home/screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:twitter_desktop/screens/home/components/sidebar/widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        overflow: Overflow.visible,
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            bottom: 0,
+            child: Sidebar(
+              width: 200,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
