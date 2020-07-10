88b4ce884e984dfec177aa824e96ce9638f7a00b
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-10 13:54:49 +0100

    Added TwitterDesktopApp

---
+++ twitter_desktop/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:twitter_desktop/router.gr.dart';
+import 'package:twitter_desktop/theme.dart';
+
+class TwitterDesktopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: ExtendedNavigator<Router>(router: Router()),
+      theme: getTheme(context, true),
+    );
+  }
+}
