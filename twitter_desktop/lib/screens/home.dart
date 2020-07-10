ce33812e74e7176e9a27f2fee4f0ef84477e0177
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-10 13:55:10 +0100

    Added home_screen.dart

---
+++ twitter_desktop/lib/screens/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //
+    );
+  }
+}
