f6224954e9c74f1833d3cf3af3f5825e80bd783c
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-10 23:21:36 +0100

    Added top_bar

--- twitter_desktop/lib/ui/screens/home/screen.dart
+++ twitter_desktop/lib/ui/screens/home/screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:twitter_desktop/ui/screens/home/components/sidebar/widget.dart';
+import 'package:twitter_desktop/ui/screens/home/components/topbar/widget.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,18 +12,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        overflow: Overflow.visible,
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            bottom: 0,
-            child: Sidebar(
-              width: 200,
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Stack(
+          overflow: Overflow.visible,
+          children: [
+            Positioned(
+              left: 200,
+              top: 0,
+              right: 0,
+              child: Topbar(),
             ),
-          ),
-        ],
+            Positioned(
+              top: 0,
+              left: 0,
+              bottom: 0,
+              child: Sidebar(
+                width: 200,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
