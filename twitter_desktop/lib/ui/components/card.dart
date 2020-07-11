bd75c064df8b94d18634098a6b5c6e5f522f0ee3
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-11 14:56:38 +0100

    Added AppCardStyle

--- twitter_desktop/lib/ui/components/card.dart
+++ twitter_desktop/lib/ui/components/card.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+enum AppCardStyle { Normal, Outlined }
+
 class AppCard extends StatelessWidget {
   final EdgeInsets margin;
   final EdgeInsets padding;
   final bool enabled;
   final Widget child;
+  final double width;
+  final double height;
+  final AppCardStyle style;
 
   const AppCard({
     Key key,
@@ -12,20 +17,33 @@ class AppCard extends StatelessWidget {
     this.padding,
     this.enabled,
     this.child,
+    this.width,
+    this.height,
+    this.style = AppCardStyle.Normal,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: width,
+      height: height,
       padding: padding,
       decoration: BoxDecoration(
-        color: Theme.of(context).backgroundColor,
+        color: style == AppCardStyle.Normal
+            ? Theme.of(context).backgroundColor
+            : Colors.transparent,
         borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          width: 2,
+          color: Theme.of(context).backgroundColor,
+        ),
         boxShadow: [
-          BoxShadow(
-            blurRadius: 15,
-            color: Colors.black.withOpacity(.2),
-          ),
+          if (style == AppCardStyle.Normal)
+            BoxShadow(
+              blurRadius: 3,
+              offset: Offset(0, 2),
+              color: Colors.black.withOpacity(.1),
+            ),
         ],
       ),
       child: child,
