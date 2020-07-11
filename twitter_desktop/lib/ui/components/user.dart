745bc1e9ad834f895cee65308bc37436ed0b1a79
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-11 14:56:48 +0100

    Added UserAvatar

---
+++ twitter_desktop/lib/ui/components/user.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class UserAvatar extends StatefulWidget {
+  final String username;
+  final String userAvatar;
+  final double width;
+  final double height;
+
+  UserAvatar({
+    Key key,
+    this.username,
+    this.userAvatar,
+    this.width = 40,
+    this.height = 40,
+  }) : super(key: key);
+
+  @override
+  _UserAvatarState createState() => _UserAvatarState();
+}
+
+class _UserAvatarState extends State<UserAvatar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (widget.username != null)
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget.username,
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(width: 15),
+            ],
+          ),
+        Container(
+          width: widget.width,
+          height: widget.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(widget.userAvatar),
+            ),
+            borderRadius: BorderRadius.circular(50),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 5,
+                color: Colors.black.withOpacity(.2),
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
