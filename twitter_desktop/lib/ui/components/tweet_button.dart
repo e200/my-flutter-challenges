cbd9fce9c349f607d5d9770951cfa378a1b99c62
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-15 02:14:42 +0100

    Added TweetButton

---
+++ twitter_desktop/lib/ui/components/tweet_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TweetButton extends StatelessWidget {
+  final EdgeInsets contentPadding;
+
+  const TweetButton({
+    Key key,
+    this.contentPadding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: contentPadding ?? EdgeInsets.all(15),
+      elevation: 0,
+      color: Theme.of(context).primaryColor,
+      child: Text(
+        'Tweet',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+}
