0c3f7c6f81616e93e3eed88c81e91a4076940e69
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-15 02:12:11 +0100

    Added app icons

---
+++ twitter_desktop/lib/ui/app_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class FlutterApp {
+  FlutterApp._();
+
+  static const _kFontFam = 'FlutterApp';
+  static const _kFontPkg = null;
+
+  static const IconData verified = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
