5e62f9235c826becf4a82cb3f1d2b42645015eb1
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-18 18:29:13 +0100

    Added title card

---
+++ twitter_desktop/lib/ui/components/title_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:twitter_desktop/bloc/theme_bloc.dart';
+import 'package:twitter_desktop/ui/components/card.dart';
+
+class TitledCard extends StatelessWidget {
+  final Widget title;
+  final double width;
+  final Widget child;
+  final EdgeInsets padding;
+
+  const TitledCard({
+    Key key,
+    this.title,
+    this.width,
+    this.child,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeBloc, bool>(
+      bloc: BlocProvider.of<ThemeBloc>(context),
+      builder: (context, isDark) {
+        return AppCard(
+          width: width,
+          padding: padding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Trends for you',
+                style: Theme.of(context).textTheme.headline6.apply(
+                      color: isDark
+                          ? Colors.white.withOpacity(.6)
+                          : Colors.black.withOpacity(.6),
+                    ),
+              ),
+              SizedBox(height: 15),
+              child,
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
