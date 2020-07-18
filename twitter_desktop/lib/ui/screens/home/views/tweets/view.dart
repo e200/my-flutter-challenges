5217a47a84b4d2a3a63dc293f5c839ef68c7ccba
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-18 20:16:41 +0100

    Moved UsersYouShouldFollow to its own file

--- twitter_desktop/lib/ui/screens/home/views/tweets/view.dart
+++ twitter_desktop/lib/ui/screens/home/views/tweets/view.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_feather_icons/flutter_feather_icons.dart';
-import 'package:twitter_desktop/bloc/theme_bloc.dart';
 import 'package:twitter_desktop/ui/components/new_tweet/widget.dart';
-import 'package:twitter_desktop/ui/components/title_card.dart';
-import 'package:twitter_desktop/ui/components/user.dart';
-import 'package:twitter_desktop/ui/components/user_verfied.dart';
-import 'package:twitter_desktop/ui/components/user_you_should_follow.dart';
+import 'package:twitter_desktop/ui/components/users_you_should_follow.dart';
 import 'package:twitter_desktop/ui/screens/home/views/tweets/list.dart';
 
 class TweetsView extends StatefulWidget {
@@ -43,100 +37,7 @@ class _TweetsViewState extends State<TweetsView> {
           ),
           SizedBox(width: 15),
           Expanded(
-            child: BlocBuilder<ThemeBloc, bool>(
-              bloc: BlocProvider.of<ThemeBloc>(context),
-              builder: (context, isDark) {
-                return ListView(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 20,
-                  ),
-                  children: [
-                    TitledCard(
-                      width: 310,
-                      padding: EdgeInsets.all(15),
-                      child: Column(
-                        children: [
-                          ListTile(
-                            contentPadding: EdgeInsets.zero,
-                            title: Text('#Flutter'),
-                            subtitle: Text(
-                              '7.6k tweets',
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            trailing: Icon(
-                              FeatherIcons.chevronDown,
-                              color: isDark
-                                  ? Colors.white.withOpacity(.6)
-                                  : Colors.black.withOpacity(.6),
-                            ),
-                            onTap: () {},
-                          ),
-                          ListTile(
-                            contentPadding: EdgeInsets.zero,
-                            title: Text('#Dart'),
-                            subtitle: Text(
-                              '3.9k tweets',
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            trailing: Icon(
-                              FeatherIcons.chevronDown,
-                              color: isDark
-                                  ? Colors.white.withOpacity(.6)
-                                  : Colors.black.withOpacity(.6),
-                            ),
-                            onTap: () {},
-                          ),
-                          ListTile(
-                            contentPadding: EdgeInsets.zero,
-                            title: Text('#FlutterDesktop'),
-                            subtitle: Text(
-                              '1.2k tweets',
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            trailing: Icon(
-                              FeatherIcons.chevronDown,
-                              color: isDark
-                                  ? Colors.white.withOpacity(.6)
-                                  : Colors.black.withOpacity(.6),
-                            ),
-                            onTap: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    TitledCard(
-                      width: 310,
-                      padding: EdgeInsets.all(15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          UserYouShouldFollow(
-                            avatar:
-                                'https://uifaces.co/our-content/donated/t33XAm04.jpg',
-                            username: 'robergd',
-                            fullname: 'Rober González',
-                          ),
-                          UserYouShouldFollow(
-                            avatar:
-                                'https://uifaces.co/our-content/donated/XdLjsJX_.jpg',
-                            username: 'sheyra',
-                            fullname: 'Sheyra',
-                            isVerified: true,
-                          ),
-                          UserYouShouldFollow(
-                            avatar:
-                                'https://uifaces.co/our-content/donated/mB6AsKYP.jpg',
-                            username: 'San_thoshJ',
-                            fullname: 'Santhosh.J',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
+            child: UsersYouShouldFollow(),
           ),
         ],
       ),
