770d5f2c7732c98ca9ab8c6e4dcf9c3919898407
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-24 17:06:36 +0100

    Fixed issue on Twitter App route generated files

---
+++ twitter_desktop/lib/ui/router.gr.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:twitter_desktop/ui/screens/home/screen.dart';
+
+class Routes {
+  static const String homeScreen = '/';
+  static const all = <String>{
+    homeScreen,
+  };
+}
+
+class Router extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(Routes.homeScreen, page: HomeScreen),
+  ];
+  @override
+  Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, AutoRouteFactory>{
+    HomeScreen: (RouteData data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => HomeScreen(),
+        settings: data,
+      );
+    },
+  };
+}
