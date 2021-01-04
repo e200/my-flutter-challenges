455446a9d8a155052310aa0b6e7d140c62a16d6f
Eleandro Duzentos <eleandro@inbox.ru>
2021-01-04 08:18:08 +0100

    Added app.dart

--- animated_mouse_pointer/test/widget_test.dart
+++ animated_mouse_pointer/test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:animated_mouse_pointer/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:animated_mouse_pointer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(AnimatedMousePointerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
