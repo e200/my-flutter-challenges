5d1f4931c74e2b61df1d8289d9d940857e8313aa
Eleandro Duzentos <eleandro@inbox.ru>
2021-01-04 08:17:52 +0100

    Added app.dart

---
+++ animated_mouse_pointer/lib/app.dart
@@ -0,0 +1,7 @@
+import 'package:animated_mouse_pointer/screens/home.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedMousePointerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: HomeScreen());
+}
