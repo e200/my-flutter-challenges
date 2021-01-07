0bc95353c0f6d8a40439c245b11d01ed62804643
Eleandro Duzentos <eleandro@inbox.ru>
2021-01-07 23:55:40 +0100

    Added MouseArea

--- animated_mouse_pointer/lib/screens/home.dart
+++ animated_mouse_pointer/lib/screens/home.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:animated_mouse_pointer/widgets/mouse_area.dart';
+
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: MouseArea(),
+    );
   }
 }
