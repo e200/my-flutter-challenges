0830896f2f797c605e3df70e80785d3e7556de7f
Eleandro Duzentos <eleandro@inbox.ru>
2021-01-08 09:28:32 +0100

    Added LayoutBuilder to define the limits of the cursor area

--- animated_mouse_pointer/lib/widgets/mouse_pointer/widget.dart
+++ animated_mouse_pointer/lib/widgets/mouse_pointer/widget.dart
@@ -3,10 +3,30 @@ import 'package:flutter/material.dart';
 import 'painter.dart';
 
 class MousePointer extends StatelessWidget {
+  final double radius;
+  final Offset position;
+
+  const MousePointer({
+    Key key,
+    this.radius,
+    this.position,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: MousePointerPaint(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return CustomPaint(
+          size: Size(
+            constraints.maxWidth,
+            constraints.maxHeight,
+          ),
+          painter: MousePointerPaint(
+            radius: radius,
+            position: position,
+          ),
+        );
+      },
     );
   }
 }
