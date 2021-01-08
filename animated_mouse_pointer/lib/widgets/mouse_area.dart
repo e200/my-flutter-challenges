9dd2eda810fb883ef0ec90427c6e13e0c9b8de8b
Eleandro Duzentos <eleandro@inbox.ru>
2021-01-08 09:27:02 +0100

    Added _pointerOffsetNotifier to track the mouse potion on the screen

--- animated_mouse_pointer/lib/widgets/mouse_area.dart
+++ animated_mouse_pointer/lib/widgets/mouse_area.dart
@@ -1,20 +1,29 @@
 import 'package:animated_mouse_pointer/widgets/mouse_pointer/widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class MouseArea extends StatefulWidget {
-  @override
-  _MouseAreaState createState() => _MouseAreaState();
-}
+class MouseArea extends StatelessWidget {
+  final _pointerOffsetNotifier = ValueNotifier<Offset>(Offset.infinite);
 
-class _MouseAreaState extends State<MouseArea> {
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onHover: (value) {},
+    return MouseRegion(
+      cursor: SystemMouseCursors.none,
+      onHover: (value) {
+        _pointerOffsetNotifier.value = value.position;
+      },
       child: Stack(
         fit: StackFit.expand,
         children: [
-          MousePointer(),
+          ValueListenableBuilder<Offset>(
+            valueListenable: _pointerOffsetNotifier,
+            builder: (context, value, child) {
+              return MousePointer(
+                radius: 10,
+                position: value,
+              );
+            }
+          ),
         ],
       ),
     );
