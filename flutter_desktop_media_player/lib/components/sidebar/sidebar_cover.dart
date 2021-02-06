60e198dc12a309c2db9149d1aa8e07cf247b646d
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-06 20:42:50 +0100

    Added image max size

--- flutter_desktop_media_player/lib/components/sidebar/sidebar_cover.dart
+++ flutter_desktop_media_player/lib/components/sidebar/sidebar_cover.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class SidebarCover extends StatelessWidget {
@@ -16,32 +17,42 @@ class SidebarCover extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: padding ?? EdgeInsets.only(
-        left: 30,
-        right: 30,
-        top: 30,
-        bottom: 10,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(.2),
-            spreadRadius: 5,
-            blurRadius: 15,
-            offset: Offset(0, 10),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: Container(
+        margin: padding ??
+            EdgeInsets.only(
+              left: 30,
+              right: 30,
+              top: 30,
+              bottom: 10,
+            ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.2),
+              spreadRadius: 5,
+              blurRadius: 15,
+              offset: Offset(0, 10),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: 500,
+              maxWidth: 500,
+            ),
+            child: Image.asset(
+              image,
+              filterQuality: FilterQuality.low,
+              fit: BoxFit.cover,
+              width: width,
+              height: height,
+            ),
           ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(15),
-        child: Image.asset(
-          image,
-          filterQuality: FilterQuality.low,
-          fit: BoxFit.cover,
-          width: width,
-          height: height,
         ),
       ),
     );
