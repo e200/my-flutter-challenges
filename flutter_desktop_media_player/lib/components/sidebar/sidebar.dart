1192c295ba5c1c0310124f0dac768134de234554
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-06 20:45:43 +0100

    Added blurred dialog for tracks preview

--- flutter_desktop_media_player/lib/components/sidebar/sidebar.dart
+++ flutter_desktop_media_player/lib/components/sidebar/sidebar.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_desktop_media_player/components/image_preview.dart';
+import 'package:flutter_desktop_media_player/data/track.dart';
+import 'package:flutter_desktop_media_player/ui/screens/track_preview.dart';
 import 'package:flutter_desktop_media_player/components/playlist/widget.dart';
 import 'package:flutter_desktop_media_player/components/sidebar/sidebar_cover.dart';
 import 'package:flutter_desktop_media_player/components/sidebar/sidebar_tile.dart';
+import 'package:flutter_desktop_media_player/ui/widgets/blur.dart';
 import 'package:line_icons/line_icons.dart';
 
 class Sidebar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _playingTrack = tracks.first;
+
     return Flexible(
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -23,6 +27,7 @@ class Sidebar extends StatelessWidget {
                   height: 300,
                   padding: EdgeInsets.all(15),
                   child: ListView(
+                    physics: BouncingScrollPhysics(),
                     shrinkWrap: true,
                     children: <Widget>[
                       SidebarTile(
@@ -33,49 +38,27 @@ class Sidebar extends StatelessWidget {
                         icon: LineIcons.inbox,
                         title: 'Browse',
                       ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      SidebarTile(
-                        title: 'Library',
-                      ),
+                      SizedBox(height: 15),
+                      SidebarTile(title: 'Library'),
                       SidebarTile(
                         title: 'Playlists',
                         isReadOnly: true,
                       ),
                       Column(
                         children: <Widget>[
-                          SidebarTile(
-                            title: 'Kuduro!!! 🔥',
-                            isActive: true,
-                          ),
-                          SidebarTile(
-                            title: 'Afro House 🦄',
-                            isActive: true,
-                          ),
-                          SidebarTile(
-                            title: 'Kizomba',
-                            isActive: true,
-                          ),
-                          SidebarTile(
-                            title: 'Most played',
-                            isActive: true,
-                          ),
-                          SidebarTile(
-                            title: 'Recently played',
-                            isActive: true,
-                          ),
-                          SidebarTile(
-                            title: 'Archive',
-                            isActive: true,
-                          ),
+                          SidebarTile(title: 'Kuduro!!! 🔥'),
+                          SidebarTile(title: 'Afro House 🦄', isActive: true),
+                          SidebarTile(title: 'Kizomba'),
+                          SidebarTile(title: 'Most played'),
+                          SidebarTile(title: 'Recently played'),
+                          SidebarTile(title: 'Archive'),
                         ],
                       ),
                     ],
                   ),
                 ),
                 Positioned(
-                  bottom: -80,
+                  bottom: 0,
                   left: 0,
                   right: 0,
                   child: Column(
@@ -85,11 +68,23 @@ class Sidebar extends StatelessWidget {
                         onTap: () {
                           Navigator.push(
                             context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return ImagePreview(
-                                    image: 'assets/babulla-cover.jpg',
-                                    tag: 'current-cover');
+                            PageRouteBuilder(
+                              opaque: false,
+                              fullscreenDialog: true,
+                              barrierDismissible: true,
+                              pageBuilder:
+                                  (context, animation, secondaryAnimation) {
+                                return TrackPreview(
+                                  track: _playingTrack,
+                                  tag: 'current-cover',
+                                );
+                              },
+                              transitionsBuilder: (context, animation,
+                                  secondaryAnimation, child) {
+                                return BlurLayer(
+                                  animation: animation,
+                                  child: child,
+                                );
                               },
                             ),
                           );
@@ -97,47 +92,17 @@ class Sidebar extends StatelessWidget {
                         child: Hero(
                           tag: 'current-cover',
                           child: SidebarCover(
-                            image: 'assets/babulla-cover.jpg',
+                            image: _playingTrack.coverUrl,
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 30,
-                          vertical: 15,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Babulla (Original Mix)',
-                              style: TextStyle(
-                                color: Color(0xFFC4C1C2),
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              'Wilson Kentura, Tiuze Money, Mil Tokes Portuga',
-                              style: TextStyle(
-                                color: Color(0xFF4E586B),
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
                     ],
                   ),
                 ),
               ],
             ),
           ),
-          Expanded(
-            child: Playlist(),
-          )
+          Expanded(child: Playlist()),
         ],
       ),
     );
