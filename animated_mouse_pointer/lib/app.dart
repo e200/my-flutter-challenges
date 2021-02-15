7ed49e6663a473d19d9d031dac91d5b78022fb23
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-15 22:43:08 +0100

    Imported pulse package

--- animated_mouse_pointer/lib/app.dart
+++ animated_mouse_pointer/lib/app.dart
@@ -1,5 +1,5 @@
-import 'package:animated_mouse_pointer/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:pulse/pulse.dart';
 
 class AnimatedMousePointerApp extends StatelessWidget {
   @override
