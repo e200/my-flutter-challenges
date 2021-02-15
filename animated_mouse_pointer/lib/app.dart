fa3e98e25754ecb98e1bded90b3153cf2d588531
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-15 22:42:22 +0100

    Added pulse widget

--- animated_mouse_pointer/lib/app.dart
+++ animated_mouse_pointer/lib/app.dart
@@ -10,3 +10,50 @@ class AnimatedMousePointerApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _colors = [
+    Colors.black,
+    Colors.white,
+  ];
+
+  final _mousePointerPositionNotifier = ValueNotifier(Offset.zero);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MouseRegion(
+        onHover: (details) {
+          _mousePointerPositionNotifier.value = details.localPosition;
+        },
+        child: Pulse(
+          pulseColor: _colors.first,
+          duration: Duration(seconds: 1),
+          child: ValueListenableBuilder(
+            valueListenable: _mousePointerPositionNotifier,
+            builder: (context, value, child) {
+              return Stack(
+                children: [
+                  MouseFollower(position: value),
+                ],
+              );
+            },
+          ),
+          onComplete: () {
+            setState(() {
+              final _currentColor = _colors.first;
+
+              _colors.remove(_currentColor);
+              _colors.add(_currentColor);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
