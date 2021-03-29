8cac1202816fb0871c96a71876693b415fd59f2d
Eleandro Duzentos <eleandro@inbox.ru>
2021-03-29 20:54:39 +0100

    Added MouseTrail widget

--- mouse_trail/lib/main.dart
+++ mouse_trail/lib/main.dart
@@ -99,3 +99,86 @@ class SourceBackgroundPainter extends CustomPainter {
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
 
+class MouseTrail extends StatefulWidget {
+  final Offset center;
+  final Random random;
+  final Size size;
+  final Function(Widget owner) onCompleteAnimation;
+
+  const MouseTrail({
+    Key key,
+    this.center,
+    this.size,
+    this.random,
+    this.onCompleteAnimation,
+  }) : super(key: key);
+
+  @override
+  _MouseTrailState createState() => _MouseTrailState();
+}
+
+class _MouseTrailState extends State<MouseTrail>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<Offset> _animation;
+
+  final _mouseTrailRadius = 50.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 7),
+    );
+
+    if (widget.onCompleteAnimation != null) {
+      _animationController.addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          widget.onCompleteAnimation(widget);
+        }
+      });
+    }
+
+    final _random = widget.random;
+
+    final _size = widget.size;
+
+    final _x = (_size.width * _random.nextDouble()) + _mouseTrailRadius;
+    final _y = (_size.height * _random.nextDouble()) + _mouseTrailRadius;
+
+    _animation = Tween<Offset>(
+      begin: widget.center,
+      end: Offset(_x, _y),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeOut,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    _animationController.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: MediaQuery.of(context).size,
+      painter: MouseTrailPainter(
+        radius: _mouseTrailRadius,
+        center: _animation.value,
+      ),
+    );
+  }
+}
+
