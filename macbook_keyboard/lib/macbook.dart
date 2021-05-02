462392ab55715622396aa7ddeb0540ce8560a57a
Eleandro Duzentos <eleandro@inbox.ru>
2021-05-02 20:02:39 +0100

    feat: added macbook key

--- macbook_keyboard/lib/macbook.dart
+++ macbook_keyboard/lib/macbook.dart
@@ -1,3 +1,47 @@
+
+class MacbookKey extends StatelessWidget {
+  final double width;
+  final double height;
+  final Widget child;
+  final Alignment alignment;
+  final Function() onPressed;
+
+  MacbookKey({
+    Key key,
+    this.width,
+    this.height,
+    this.child,
+    this.alignment,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: DefaultTextStyle(
+        style: DefaultTextStyle.of(context).style.copyWith(color: Colors.white),
+        child: Container(
+          alignment: alignment,
+          width: width,
+          height: height,
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF23272D),
+                Color(0xFF1C1B1E),
+              ],
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
 class TrackPad extends StatelessWidget {
   final Color color;
 
