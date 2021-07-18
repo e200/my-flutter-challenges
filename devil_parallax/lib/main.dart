bb0955ff163e165b62b2f3af6f07c426833fb339
Eleandro Duzentos <hi@e200.dev>
2021-07-18 13:14:53 +0100

    feat: added PerspectiveLayer into the parallax stack

--- devil_parallax/lib/main.dart
+++ devil_parallax/lib/main.dart
@@ -42,29 +42,32 @@ class ParallaxDevil extends StatelessWidget {
         onExit: (event) {
           _localOffsetNotifier.value = Offset.zero;
         },
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: List.generate(
+                    5,
+                    (index) {
+                      return PerspectiveLayer(
+                        borderRadius: BorderRadius.circular(30),
+                        offset: Offset(
+                          (index * .03) * value.dx,
+                          (index * .08) * value.dy,
+                        ),
+                        child: Transform(
+                          alignment: FractionalOffset.center,
+                          transform: Matrix4.identity()
+                            ..setEntry(3, 2, 0.002)
+                            ..rotateY(0.0003 * value.dy)
+                            ..rotateX(0.0003 * value.dx),
+                          child: Image.asset(
+                            'assets/$index.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
