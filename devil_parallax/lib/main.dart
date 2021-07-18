74a30ec6f6f6189eb2b64822fe234823725c5f56
Eleandro Duzentos <hi@e200.dev>
2021-07-18 13:07:37 +0100

    feat: added ParallaxDevilApp

--- devil_parallax/lib/main.dart
+++ devil_parallax/lib/main.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ParallaxDevilApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ParallaxDevilApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Parallax Devil',
     );
   }
 }
