6f331a1362fff9899af27dccbbbff31d2db59995
Eleandro Duzentos <hi@e200.dev>
2021-07-19 00:00:52 +0100

    refactor: removed unnecessary ConstrainedBox widget feat: added fit: BoxFit.cover to the images

--- devil_parallax/lib/main.dart
+++ devil_parallax/lib/main.dart
@@ -75,49 +75,48 @@ class _ParallaxDevilState extends State<ParallaxDevil>
               builder: (context, _) {
                 final value = _animation.value;
 
-                return ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxWidth: 1024,
-                    maxHeight: 576,
-                  ),
-                  child: Transform(
-                    alignment: FractionalOffset.center,
-                    transform: Matrix4.identity()
-                      ..setEntry(3, 2, 0.002)
-                      ..rotateY(0.0003 * value.dy)
-                      ..rotateX(0.0003 * value.dx),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(.3),
-                            spreadRadius: 5,
-                            blurRadius: 15,
-                          ),
-                        ],
-                      ),
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: List.generate(
-                          5,
-                          (index) {
-                            return PerspectiveLayer(
-                              offset: Offset(
-                                (index * .03) * value.dx,
-                                (index * .08) * value.dy,
-                              ),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/$index.png'),
-                                  ),
+                return Transform(
+                  alignment: FractionalOffset.center,
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.002)
+                    ..rotateY(0.0003 * value.dy)
+                    ..rotateX(0.0003 * value.dx),
+                  child: Container(
+                    constraints: BoxConstraints(
+                      maxWidth: 1024,
+                      maxHeight: 576,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(.3),
+                          spreadRadius: 5,
+                          blurRadius: 15,
+                        ),
+                      ],
+                    ),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: List.generate(
+                        5,
+                        (index) {
+                          return PerspectiveLayer(
+                            offset: Offset(
+                              (index * .03) * value.dx,
+                              (index * .08) * value.dy,
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                image: DecorationImage(
+                                  image: AssetImage('assets/$index.png'),
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                            );
-                          },
-                        ),
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ),
