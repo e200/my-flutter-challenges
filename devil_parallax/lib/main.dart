d90cc667150b08901ed39f9b060d6c46b6af0aa5
Eleandro Duzentos <hi@e200.dev>
2021-07-18 13:13:22 +0100

    feat: added _localOffsetNotifier notifier

--- devil_parallax/lib/main.dart
+++ devil_parallax/lib/main.dart
@@ -17,34 +17,7 @@ class ParallaxDevilApp extends StatelessWidget {
 class ParallaxDevil extends StatelessWidget {
   ParallaxDevil({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _localOffsetNotifier = ValueNotifier(Offset.zero);
 
   @override
   Widget build(BuildContext context) {
